5153ccdae9d91480aa877c0bc98ce8712fb20206
Lukas Hofer <2310859020@hochschule-burgenland.at>
2025-01-19 14:48:31 +0100

    Set registration date when creating a new registration

--- src/app/dashboard/add-data/add-data.component.ts
+++ src/app/dashboard/add-data/add-data.component.ts
@@ -1,14 +1,14 @@
-import {Component, OnInit} from '@angular/core';
-import {SharedModule} from '../../shared/shared.module';
-import {FormBuilder, FormGroupDirective, Validators} from '@angular/forms';
-import {StoreService} from '../../shared/store.service';
-import {BackendService} from '../../shared/backend.service';
-import {MatInputModule} from "@angular/material/input";
-import {MatSelectModule} from "@angular/material/select";
-import {MatCheckboxModule} from "@angular/material/checkbox";
-import {MatButtonModule} from "@angular/material/button";
-import {MatDialog} from "@angular/material/dialog";
-import {SuccessDialogComponent} from "./success-dialog/success-dialog.component";
+import { Component, OnInit } from '@angular/core';
+import { SharedModule } from '../../shared/shared.module';
+import { FormBuilder, FormGroupDirective, Validators } from '@angular/forms';
+import { StoreService } from '../../shared/store.service';
+import { BackendService } from '../../shared/backend.service';
+import { MatInputModule } from "@angular/material/input";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDialog } from "@angular/material/dialog";
+import { SuccessDialogComponent } from "./success-dialog/success-dialog.component";
 
 @Component({
   selector: 'app-add-data',
@@ -28,13 +28,15 @@ export class AddDataComponent implements OnInit {
       name: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(100)]],
       birthdate: [null, Validators.required],
       courseId: ['', Validators.required],
+      registrationDate: [null],
       emailSubscription: [false]
     })
   }
 
   onSubmit(form: FormGroupDirective) {
     if (this.registrationForm.valid) {
-      this.backendService.addRegistration(this.registrationForm.value, this.storeService.currentPage);
+      const registration = {...this.registrationForm.value, registrationDate: new Date()}
+      this.backendService.addRegistration(registration, this.storeService.currentPage);
       this.dialog.open(SuccessDialogComponent, {
         data: {course: this.getCourseName(this.registrationForm.value.courseId)}
       });
