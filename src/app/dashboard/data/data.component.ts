c3d2a60cf618bf218cd1cc1576929bd2efb9d0a3
Lukas Hofer <2310859020@hochschule-burgenland.at>
2025-01-19 23:06:11 +0100

    Introduce filter functionality for courses (UX Improvement)

--- src/app/dashboard/data/data.component.ts
+++ src/app/dashboard/data/data.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 import { SharedModule } from '../../shared/shared.module';
 import { StoreService } from '../../shared/store.service';
 import { BackendService } from '../../shared/backend.service';
-import {MatButtonModule} from "@angular/material/button";
+import { MatButtonModule } from "@angular/material/button";
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Registration } from '../../shared/Interfaces/Registration';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-data',
@@ -14,16 +17,21 @@ import { MatExpansionModule } from '@angular/material/expansion';
     SharedModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatExpansionModule
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
   ],
   templateUrl: './data.component.html',
   styleUrl: './data.component.css'
 })
 export class DataComponent {
 
-  constructor(public storeService: StoreService, private backendService: BackendService) {}
+  constructor(public storeService: StoreService, private backendService: BackendService) {
+  }
 
   public page: number = 0;
+  public filter = "";
 
   selectPage(i: any) {
     let currentPage = i;
@@ -35,8 +43,8 @@ export class DataComponent {
     var pagesCount = Math.ceil(this.storeService.registrationTotalCount / 2);
     let res = [];
     for (let i = 0; i < pagesCount; i++) {
-        res.push(i + 1);
-      }
+      res.push(i + 1);
+    }
     return res;
   }
 
@@ -48,5 +56,13 @@ export class DataComponent {
   changeOrder() {
     this.storeService.changeOrder();
     this.backendService.getRegistrations(this.page);
+  }
+
+  filteredCourses() {
+    return this.storeService.courses.filter(item =>
+      item.name.toLowerCase().includes(this.filter.toLowerCase()) ||
+      item.instructor.toLowerCase().includes(this.filter.toLowerCase()) ||
+      item.eventLocation.name.toLowerCase().includes(this.filter.toLowerCase()) ||
+      item.eventLocation.address.toLowerCase().includes(this.filter.toLowerCase()));
   }
 }
