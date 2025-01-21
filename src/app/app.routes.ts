71994c516c9e7e764f450b68c2585d9cb864a321
Lukas Hofer <2310859020@hochschule-burgenland.at>
2025-01-21 23:38:30 +0100

    Add FAQ page (UX Improvement)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { AboutPageComponent } from './about-page/about-page.component';
+import { FaqPageComponent } from './faq-page/faq-page.component';
 
 export const routes: Routes = [
     { path: '', component: DashboardComponent },
     { path: 'dashboard', component: DashboardComponent },
-    { path: 'about', component: AboutPageComponent }
+    { path: 'about', component: AboutPageComponent },
+    { path: 'faq', component: FaqPageComponent }
 ];
